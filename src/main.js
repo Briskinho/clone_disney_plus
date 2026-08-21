5a64b4d20e2e98f5be8653b5b2b9e26d7484fba8
Weslley Briski <weslley_greyson6@hotmail.com>
2026-08-21 17:55:35 -0300

    add: header and fix:z-index

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
-    
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        }
+        else {
+            exibeElementosDoHeader();
+        }
+    })
+
+
+    //seção de atrações, programação das abas.
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,25 +28,38 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    //seção faq, accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
 
+//adiciona a classe is hidden quando o scroll for até o final do hero
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+//faq, abre e fecha itens
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
     const elementoPai = elemento.target.parentNode;
 
     elementoPai.classList.toggle(classe);
 }
-
+//abas seção atrações, remove botão ativos
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active')
     }
 }
-
+//abas seção atrações, esconde as abas
 function escondeTodosAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
     
